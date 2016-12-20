c3a8cd2d99d741ed14a99e2bdfd58c944526c452
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2016-12-20 11:05:19 +0400

    some little changes after launch website

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('styles', function() {
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 		.pipe(gulp.dest('dist/assets/css'))
     // .pipe(rgbapng())
-    .pipe(gulp.dest('dist/assets/css'))
+    // .pipe(gulp.dest('dist/assets/css'))
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(minifycss({
       compatibility: 'ie8',
